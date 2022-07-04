5db2e691d17278adee4bbf2f2833d011b32bd6c6
esubalew <esubalew.a2009@gmail.com>
2022-07-04 23:22:26 +0300

    improvments

--- public/partials/donors/index.php
+++ public/partials/donors/index.php
@@ -10,10 +10,10 @@
             <div class="events-row row">
                 <div class="col-md-4">
                     <div class="event-card">
-                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/water_aid_canada.png" alt="">
+                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/austrianaid.jpg" alt="">
                         <div class="event-detail">
-                            <h4>Water AID canada</h4>
-                            <p>Estibulum at maximus neque. Etiam interdum fermentum dolor, eget placerat lorem.</p>
+                            <h4>Cbm Australian</h4>
+                            <p>upporting Integrated Community Based Disability prevention and rehabilitation  services for Person with disabilities since 2004, primarily in all kebeles of Jimma, Agaro, Bedle and Metu town and currently at  Nada, Asendabo and Yayo towns of Oromia regional state addressing 930 direct beneficiaries and 5,000 families indirectly in its five year project duration( 2017-2021).</p>
 
                             <h6>25 <sup>Th</sup> Mar 2020</h6>
                         </div>
@@ -31,10 +31,10 @@
                 </div>
                  <div class="col-md-4">
                     <div class="event-card">
-                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/CBM.png" alt="">
+                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/sportsforgood.jpg" alt="">
                         <div class="event-detail">
-                            <h4>CBM</h4>
-                            <p>Estibulum at maximus neque. Etiam interdum fermentum dolor, eget placerat lorem.</p>
+                            <h4>Laureaus Sport for Good</h4>
+                            <p>upporting sport for development programmes that enhance emotional wellbeing and inspire healthy behavior change of children and young people in disadvantaged communities, reduce the impact of violence, conflict and discrimination in their lives and increase their educational achievements and employability skills. </p>
 
                             <h6>25 <sup>Th</sup> Mar 2020</h6>
                         </div>
@@ -52,10 +52,10 @@
                 </div>
                  <div class="col-md-4">
                     <div class="event-card">
-                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/COMIC-RELIEF-IN-INSPIRATION.png" alt="">
+                        <img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/donors/irish-aid.jpg" alt="">
                         <div class="event-detail">
-                            <h4>COMIC  RELIEF-IN-INSPIRATION </h4>
-                            <p>Estibulum at maximus neque. Etiam interdum fermentum dolor, eget placerat lorem.</p>
+                            <h4>Irish Aid </h4>
+                            <p>Bridge the Gap Community Based Inclusive Development is a project funded by Irish government (via Irish Aid) through Cbm Ireland and implemented by Cheshire foundation Action for Inclusion in Amhara National Regional States South Gondar Zone in three districts namely Dera, Woreata town and Debre Tabor town. </p>
 
                             <h6>25 <sup>Th</sup> Mar 2020</h6>
                         </div>
--- public/partials/home/slider.php
+++ public/partials/home/slider.php
@@ -8,13 +8,13 @@
         <div class="owl-carousel ">
             <div class="slider-img">
                 <div class="item">
-                    <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/gallery/g1.jpg" alt=""></div>
+                    <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/awareness_7.png" alt=""></div>
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
                                 <div class="animated bounceInDown slider-captions">
-                                    <h1 class="slider-title">We Care More than 1000 Students Education</h1>
-                                    <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                                    <h1 class="slider-title">We care for providing equal access to opportunities and resources for people who might otherwise be excluded or marginalized</h1>
+                                    <!-- <p class="slider-text hidden-xs">We care for providing equal access to opportunities and resources for people who might otherwise be excluded or marginalized</p> -->
                                     
                                 </div>
                             </div>
@@ -23,13 +23,13 @@
                 </div>
             </div>
             <div class="item">
-                <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/gallery/g2.jpg" alt=""></div>
+                <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/slider/SLIDE14.jpg" alt=""></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
                             <div class="slider-captions ">
                                 <h1 class="slider-title">It's time for better help.</h1>
-                                <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                                <!-- <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p> -->
                                 
                             </div>
                         </div>
@@ -37,13 +37,13 @@
                 </div>
             </div>
             <div class="item">
-                <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/gallery/g3.jpg" alt=""></div>
+                <div class="slider-img"><img src="<?php echo ds_cheshirefai_PLAGIN_URL; ?>/assets/images/slider/SLIDE13.jpg" alt=""></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
                             <div class="slider-captions ">
-                                <h1 class="slider-title">Sponser a Child Today</h1>
-                                <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                                <h1 class="slider-title">Sponsor a disability prevention, rehabilitation and inclusion, mainstreaming activities today</h1>
+                                <!-- <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p> -->
                                
                             </div>
                         </div>
