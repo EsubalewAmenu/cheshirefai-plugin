9f4d37dccf30cfcfd66cfde4bca90ab44cfe99db
esubalew <esubalew.a2009@gmail.com>
2022-07-04 05:25:47 +0300

    map location

--- public/partials/contact_us/map.php
+++ public/partials/contact_us/map.php
@@ -4,7 +4,7 @@
 
       <div style="margin-top:0px;" class="row no-margin">
 
-<iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.790821823369!2d38.77103861491151!3d8.991386493546933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8553c51c8ec1%3A0xc39017cd0970556d!2sWelo%20sefer!5e0!3m2!1sen!2set!4v1656763923636!5m2!1sen!2set"  height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe style="width:100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4967.240934939074!2d38.72714539999999!3d8.9766117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b870e68cdf741%3A0x4b57e0cdfd7aa106!2sCheshire%20Foundation%20Action%20For%20Inclusion!5e1!3m2!1sen!2set!4v1656901424786!5m2!1sen!2set"  height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 
 </div>
