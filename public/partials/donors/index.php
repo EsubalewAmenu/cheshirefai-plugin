0a5737da79fdcbb78e9c4d34f7b709d4c6d353f3
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-07-05 11:33:32 +0300

    ticket removed

--- public/partials/donors/index.php
+++ public/partials/donors/index.php
@@ -15,9 +15,9 @@
                             <h4>Cbm Australian</h4>
                             <p>upporting Integrated Community Based Disability prevention and rehabilitation  services for Person with disabilities since 2004, primarily in all kebeles of Jimma, Agaro, Bedle and Metu town and currently at  Nada, Asendabo and Yayo towns of Oromia regional state addressing 930 direct beneficiaries and 5,000 families indirectly in its five year project duration( 2017-2021).</p>
 
-                            <h6>25 <sup>Th</sup> Mar 2020</h6>
+                            <!-- <h6>25 <sup>Th</sup> Mar 2020</h6> -->
                         </div>
-                        <div class="plac-dat row no-margin">
+                        <!-- <div class="plac-dat row no-margin">
                             <div class="col-7 colplac colplacll">
                                 <strong>PLACE:</strong>
                                 <p>EASTER COAST HOTEL, CA</p>
@@ -26,7 +26,7 @@
                                  <strong>TICKET:</strong>
                                 <p>FREE OF COST</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                  <div class="col-md-4">
@@ -36,9 +36,9 @@
                             <h4>Laureaus Sport for Good</h4>
                             <p>upporting sport for development programmes that enhance emotional wellbeing and inspire healthy behavior change of children and young people in disadvantaged communities, reduce the impact of violence, conflict and discrimination in their lives and increase their educational achievements and employability skills. </p>
 
-                            <h6>25 <sup>Th</sup> Mar 2020</h6>
+                            <!-- <h6>25 <sup>Th</sup> Mar 2020</h6> -->
                         </div>
-                        <div class="plac-dat row no-margin">
+                        <!-- <div class="plac-dat row no-margin">
                             <div class="col-7 colplac colplacll">
                                 <strong>PLACE:</strong>
                                 <p>EASTER COAST HOTEL, CA</p>
@@ -47,7 +47,7 @@
                                  <strong>TICKET:</strong>
                                 <p>FREE OF COST</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                  <div class="col-md-4">
@@ -57,9 +57,9 @@
                             <h4>Irish Aid </h4>
                             <p>Bridge the Gap Community Based Inclusive Development is a project funded by Irish government (via Irish Aid) through Cbm Ireland and implemented by Cheshire foundation Action for Inclusion in Amhara National Regional States South Gondar Zone in three districts namely Dera, Woreata town and Debre Tabor town. </p>
 
-                            <h6>25 <sup>Th</sup> Mar 2020</h6>
+                            <!-- <h6>25 <sup>Th</sup> Mar 2020</h6> -->
                         </div>
-                        <div class="plac-dat row no-margin">
+                        <!-- <div class="plac-dat row no-margin">
                             <div class="col-7 colplac colplacll">
                                 <strong>PLACE:</strong>
                                 <p>EASTER COAST HOTEL, CA</p>
@@ -68,7 +68,7 @@
                                  <strong>TICKET:</strong>
                                 <p>FREE OF COST</p>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
